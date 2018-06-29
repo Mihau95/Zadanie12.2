fbe57bded591af898f2e6cb63bb13a8d69da85bf
Mihau95 <misiek0019@wp.pl>
2018-06-29 13:13:08 +0200

    Zadanie 12.2

---
+++ src/Numbers.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Queue <Integer> numbers = new LinkedList<>();
+        numbers.offer(2);
+        numbers.offer(4);
+        numbers.offer(23);
+        numbers.offer(-13);
+        numbers.offer(10);
+        numbers.offer(20);
+        numbers.offer(90);
+        numbers.offer(-20);
+        numbers.offer(15);
+        numbers.offer(-5);
+
+
+    }
+}
+
